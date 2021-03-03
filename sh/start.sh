1adf48d75f51206f3f7c887d9d51d1927fe1e912
EC2 Default User <ec2-user@ip-172-31-90-212.ec2.internal>
2021-03-03 02:52:52 +0000

    added cabal and start.sh

---
+++ sh/start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+# Full credit for this script goes to: https://github.com/alessandrokonrad/Pi-Pool
+# This script installs all necessary dependencies to build the the cardano-node and cardano-cli binaries
+
+echo "Updating Ubuntu"
+echo
+sudo apt-get update
+sudo apt-get upgrade
+echo
+echo "Installing dependencies"
+echo
+sudo apt-get install libsodium-dev build-essential pkg-config libffi-dev libgmp-dev libssl-dev libtinfo-dev libsystemd-dev zlib1g-dev make g++ tmux git jq wget libncursesw5 llvm -y
+echo
+echo "Installing Haskell platform"
+echo
+sudo apt-get install -y haskell-platform
+wget https://github.com/alessandrokonrad/Pi-Pool/raw/master/aarch64/cabal3.2/cabal
+chmod +x cabal
+mkdir -p ~/.local/bin
+mv cabal ~/.local/bin
+sudo rm /usr/bin/cabal
+echo 'export PATH="~/.local/bin:$PATH"' >> .bashrc
+source ~/.bashrc
+echo
+echo "Done!"
